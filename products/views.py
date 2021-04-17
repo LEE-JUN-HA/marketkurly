9a855b725ba26c1678e090a8676f3ecdfa41b2d5
이준하 <ijunha@ijunhaui-MacBookPro.local>
2021-04-17 18:13:13 +0900

    CREATE : productlist 완료

--- products/views.py
+++ products/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http  import JsonResponse
+from django.views import View
+
+from users.models      import User
+# from users.utils     import login_decorator
+from products.models   import Product
+
+class ProductListView(View):
+    # @login_decorator
+    def get(self, request):
+        products = Product.objects.all()
+        
+        product_list = []
+        for product in products:
+            dict = {
+                'id': product.id,
+                'name': product.name,
+                'sub_name': product.sub_name,
+            }
+            product_list.append(dict)
+        return JsonResponse({"product": product_list}, status=200)
