b06febc3f50ea605adc8d94002ea475d8649bb53
이준하 <ijunha@ijunhaui-MacBookPro.local>
2021-04-26 14:37:44 +0900

    Create : ProductDetail

--- products/views.py
+++ products/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http  import JsonResponse
+from django.views import View
+
+from users.models      import User
+# from users.utils     import login_decorator
+from products.models   import Product
+
+class ProductDetailView(View):
+    # @login_decorator
+    def get(self, request):
+        products = Product.objects.all()
+
+        product_list = []
+        for product in products:
+            dict = {
+                'id'            : product.id,
+                'name'          : product.name,
+                'sub_name'      : product.sub_name,
+                'price'         : product.price,
+                'cotent'        : product.content,
+                'unit_of_sale'  : product.unit_of_sale,
+                'volume'        : product.volume,
+                'delivery_type' : product.delivery_type,
+                'packing_type'  : product.packing_type,
+                'image_url'     : [productimage.image_url for productimage in product.productimage_set.all()],
+            }
+            product_list.append(dict)
+        return JsonResponse({"products": product_detail}, status=200) 
