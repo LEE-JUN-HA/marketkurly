f69ca579851484da4dc5f04dbbd72d7c5bc45c2e
이준하 <ijunha@ijunhaui-MacBookPro.local>
2021-04-28 15:51:41 +0900

    ADD : cart(작성 중)

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http      import JsonResponse
+from django.views     import View
+
+from user.models    import User
+from user.utils     import login_decorator
+from product.models import (
+    OrderItem,
+)
+from order.models   import OrderProduct, Order, OrderStatus
+# from user.utils     import login_decorator
+
+class CartView(View):
+    # @login_decorator
+    def post(self, request, *args, **kwargs):
+        try:
+            data         = json.loads(request.body)
+            user         = request.user
+            product_id   = data['productId']
+            total_price  = data['totalPrice']
+            quantity     = data['quantity']
+            order_status = OrderStatus.objects.get(name=SHOPPING_BASKET)
